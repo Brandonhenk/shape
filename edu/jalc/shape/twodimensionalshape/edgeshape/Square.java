5e68345e484f9482345690ad0a1a98c6f6dec55a
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-03-27 12:13:09 -0500

    Added Ellipse

---
+++ edu/jalc/shape/twodimensionalshape/edgeshape/Square.java
@@ -0,0 +1,7 @@
+package edu.jalc.shape.twodimensionalshape.edgeshape;
+
+public final class Square extends Rectangle{
+  public Square(double side){
+    super(side);
+  }
+}
---
+++ edu/jalc/shape/twodimensionalshape/ellipseshape/Ellipse.java
@@ -0,0 +1,12 @@
+package edu.jalc.shape.twodimensionalshape.ellipseshape;
+
+public class Ellipse extends EllipseShape{
+  public Ellipse(double minorAxis, double majorAxis){
+    super(minorAxis, majorAxis);
+  }
+  public double getCircumference(){
+    double minorAxis = this.getMinorAxis();
+    double majorAxis = this.getMajorAxis();
+    return 2 * Math.PI * Math.sqrt((minorAxis * minorAxis + majorAxis * majorAxis)/ 2);
+  }
+}
