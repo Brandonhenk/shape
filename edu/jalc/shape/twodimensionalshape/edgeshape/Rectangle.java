249d243214218dd91adf0d9d3d4b45a5d9770dc5
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-03-27 11:58:21 -0500

    Added Perimeter method to Rectanlge

--- edu/jalc/shape/twodimensionalshape/edgeshape/Rectangle.java
+++ edu/jalc/shape/twodimensionalshape/edgeshape/Rectangle.java
@@ -1,5 +1,14 @@
 package edu.jalc.shape.twodimensionalshape.edgeshape;
 
 public class Rectangle extends EdgeShape{
+  public Rectangle(double width, double height){
+    super(width,height);
+  }
 
+  public final double getArea(){
+    return this.getWidth() * this.getHeight();
+  }
+  public final double getPerimeter(){
+    return 2 * this.getWidth() + 2 * this.getHeight();
+  }
 }
