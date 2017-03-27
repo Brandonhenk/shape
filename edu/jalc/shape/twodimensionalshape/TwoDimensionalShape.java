6a9b65f2e8488e39f66d98027b75a70739541249
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-03-27 12:34:50 -0500

    Added equals method to 2DShape

--- edu/jalc/shape/twodimensionalshape/TwoDimensionalShape.java
+++ edu/jalc/shape/twodimensionalshape/TwoDimensionalShape.java
@@ -3,4 +3,8 @@ package edu.jalc.shape.twodimensionalshape;
 abstract public class TwoDimensionalShape{
   abstract public double getArea()
 
+  public final boolean equals(TwoDimensionalShape other){
+    return this.getArea() == other.getArea();
+  }
+
 }
